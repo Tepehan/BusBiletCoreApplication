004f4720eb50cee9f9591252711b9627101c460d
helinlacin <114211908+helinlacin@users.noreply.github.com>
2022-12-29 15:25:08 +0300

    IKullaniciServis,IkullaniciDal,KullaniciManager oluşturuldu.

---
+++ BusinessLayer/IKullaniciServis.cs
@@ -0,0 +1,19 @@
+﻿using EntityLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public interface IKullaniciServis
+    {
+        void KullaniciEkle(Kullanici kullanici);
+        void KullaniciSil(Kullanici kullanici);
+        void KullaniciGüncelle(Kullanici kullanici);
+        List<Kullanici> KullaniciListele();
+        Kullanici KullaniciGetirById(int id);
+        Kullanici KullaniciGetirByName(string name);
+    }
+}
---
+++ BusinessLayer/KullaniciManager.cs
@@ -0,0 +1,52 @@
+﻿using DataAccessLayer.Abstract;
+using EntityLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class KullaniciManager : IKullaniciServis
+    {
+
+        IKullaniciDal _kullaniciDal;
+        public KullaniciManager(IKullaniciDal kullaniciDal)
+        {
+            _kullaniciDal = kullaniciDal;
+        }
+
+        public void KullaniciEkle(Kullanici kullanici)
+        {
+            _kullaniciDal.insert(kullanici);
+        }
+
+        public Kullanici KullaniciGetirById(int id)
+        {
+            return _kullaniciDal.get(x => x.kullaniciId == id);
+
+        }
+
+        public Kullanici KullaniciGetirByName(string name)
+        {
+            return _kullaniciDal.get(x => x.kullaniciAd ==name);
+        }
+
+        public void KullaniciGüncelle(Kullanici kullanici)
+        {
+
+            _kullaniciDal.update(kullanici);
+        }
+
+        public void KullaniciSil(Kullanici kullanici)
+        {
+            _kullaniciDal.delete(kullanici);
+        }
+
+        public List<Kullanici> KullaniciListele()
+        {
+            return _kullaniciDal.list();
+        }
+    }
+}
