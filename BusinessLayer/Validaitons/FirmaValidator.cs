30fe91b777e445481c0dfd486846fb3dccf25450
helinlacin <114211908+helinlacin@users.noreply.github.com>
2022-12-30 22:05:05 +0300

    firmavalidator mesaj kısmı guncellendi

--- BusinessLayer/Validaitons/FirmaValidator.cs
+++ BusinessLayer/Validaitons/FirmaValidator.cs
@@ -9,16 +9,16 @@ namespace BusBiletCoreApplication.Validaitons
         {   
             //Rule for FirmaAd
             RuleFor(firma => firma.firmaAd).NotEmpty().WithMessage("Boş geçilemez");
-            RuleFor(firma => firma.firmaAd).MaximumLength(50).WithMessage("Maximum 50 karakter girilebilir.");
-            RuleFor(firma => firma.firmaAd).MinimumLength(2).WithMessage("Minimum 2 karakter girilmedilir.");
+            RuleFor(firma => firma.firmaAd).MaximumLength(50).WithMessage("Maximum 50 karakter girilmelidir.");
+            RuleFor(firma => firma.firmaAd).MinimumLength(2).WithMessage("Minimum 2 karakter girilmelidir.");
 
             //Rule for iletişim
-            RuleFor(firma => firma.iletisim).MaximumLength(300).WithMessage("Maximum 300 karakter girilebilir.");
+            RuleFor(firma => firma.iletisim).MaximumLength(300).WithMessage("Maximum 300 karakter girilmelidir.");
 
             //Rule for logo
             RuleFor(firma => firma.logoUrl).NotEmpty().WithMessage("Boş geçilemez");
-            RuleFor(firma => firma.logoUrl).MaximumLength(100).WithMessage("Maximum 100 karakter girilebilir.");
-            RuleFor(firma => firma.logoUrl).MinimumLength(1).WithMessage("Minimum 1 karakter girilmedilir.");
+            RuleFor(firma => firma.logoUrl).MaximumLength(100).WithMessage("Maximum 100 karakter girilmelidir.");
+            RuleFor(firma => firma.logoUrl).MinimumLength(1).WithMessage("Minimum 1 karakter girilmelidir.");
 
 
         }
