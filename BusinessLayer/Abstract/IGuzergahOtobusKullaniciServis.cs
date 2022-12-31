7e633da23f6f4517518307f7777e4b3ef5c553e8
Yusufhan-Erbas <erbasyusufhan15@gmail.com>
2022-12-31 14:02:37 +0300

    GuzergahOtobusKullanici metodları tanımlandı ve implemente edildi.

---
+++ BusinessLayer/Abstract/IGuzergahOtobusKullaniciServis.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IGuzergahOtobusKullaniciServis
+    {
+        void GuzergahOtobusKullaniciEkle(GuzergahOtobusKullanici guzergahOtobusKullanici);
+        void GuzergahOtobusKullaniciSil(GuzergahOtobusKullanici guzergahOtobusKullanici);
+        void GuzergahOtobusKullaniciGuncelle(GuzergahOtobusKullanici guzergahOtobusKullanici);
+        List<GuzergahOtobusKullanici> guzergahOtobusKullaniciListele();
+        public GuzergahOtobusKullanici guzergahOtobusKullaniciGetById(int id);
+    }
+}
---
+++ BusinessLayer/Concrete/GuzergahOtobusKullaniciManager.cs
@@ -0,0 +1,49 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class GuzergahOtobusKullaniciManager : IGuzergahOtobusKullaniciServis
+    {
+        IGuzergahOtobusKullaniciDal guzergahOtobusKullaniciDal;
+        public GuzergahOtobusKullaniciManager(IGuzergahOtobusKullaniciDal guzergahOtobusKullaniciDal)
+        {
+            this.guzergahOtobusKullaniciDal = guzergahOtobusKullaniciDal;
+        }
+
+        public void GuzergahOtobusKullaniciEkle(GuzergahOtobusKullanici guzergahOtobusKullanici)
+        {
+            guzergahOtobusKullaniciDal.insert(guzergahOtobusKullanici);
+        }
+
+
+        public GuzergahOtobusKullanici guzergahOtobusKullaniciGetById(int id)
+        {
+            return guzergahOtobusKullaniciDal.get(x => x.biletId == id);
+        }
+
+
+        public void GuzergahOtobusKullaniciGuncelle(GuzergahOtobusKullanici guzergahOtobusKullanici)
+        {
+            guzergahOtobusKullaniciDal.update(guzergahOtobusKullanici);
+        }
+
+
+        public List<GuzergahOtobusKullanici> guzergahOtobusKullaniciListele()
+        {
+            return guzergahOtobusKullaniciDal.list();
+        }
+
+
+        public void GuzergahOtobusKullaniciSil(GuzergahOtobusKullanici guzergahOtobusKullanici)
+        {
+            guzergahOtobusKullaniciDal.delete(guzergahOtobusKullanici); 
+        }
+    }
+}
